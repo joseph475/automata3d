6f306201215bb20938a3cf3832444c6673841670
joseph475 <markjoseph475@gmail.com>
2022-01-28 08:32:57 +0800

    added some pictures

--- resources/views/guest/pages/careers.blade.php
+++ resources/views/guest/pages/careers.blade.php
@@ -18,6 +18,16 @@
                     </div>
                 </div>
             </div>
+            <div class="second-section">
+                <img id="controller-container" src="{{ url('images/Shape_21.png') }}" alt="">
+                <img id="img_1" src="{{ url('images/layer_4.png') }}" alt="">
+                <div class="text-content">
+                    <h3 class="mb5">Perks and Benefits<br>
+                        Development</h3>
+                    <p>Automata offers a wider range of outstanding benefits that allow each employee to tailor a benefits package that meets his or her specific needs.</p>
+                    <p>We work in a dynamic environment and all of our employees our guaranteed to acquire new skills and improve their personal development. We offer competitive salary and lot of time off.</p>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/guest/pages/index.blade.php
+++ resources/views/guest/pages/index.blade.php
@@ -5,62 +5,63 @@
 @section('page_title', $data['meta_tags'][0]->page) --}}
 
 @section('content')
-    <div class="content">
-        <div class="first-section">
-            @include('guest.layouts.header')
-            <div class="center-vertically-horizontally">
-                <h3 class="white-text text-center font-bold mb5 pb5">Powerful Web3 Development<br>
-                    for advanced technology</h3>
-                <div class="btn-container">
-                    <img class="bg-2" src="{{ url('images/layer_2.png') }}" alt="">
+    <div id="home">
+        <div class="content">
+            <div class="first-section">
+                @include('guest.layouts.header')
+                <div class="center-vertically-horizontally">
+                    <h3 class="white-text text-center font-bold mb5 pb5">Powerful Web3 Development<br>
+                        for advanced technology</h3>
+                    <div class="btn-container">
+                        <img class="bg-2" src="{{ url('images/layer_2.png') }}" alt="">
+                        <a href="#" class="button ribbon-outset border">GET IN TOUCH</a>
+                    </div>
+
+                </div>
+            </div>
+            <div class="second-section">
+                <img id="controller-container" src="{{ url('images/Shape_21.png') }}" alt="">
+                <img id="img_1" src="{{ url('images/controller_1.png') }}" alt="">
+                <div class="text-content">
+                    <h3 class="mb5">Blockchain GameFi<br>
+                        Development</h3>
+                    <p>Automata develops games that combine video games with blockchain technology, cryptocurrencies and
+                        decentralized finance also known as GameFi.</p>
+                    <p>With focus on play-to-earn (P2E), we are developing in-game economies using cryptocurrencies,
+                        non-fungible tokens (NFTs), in-game assets, and elements of decentralized finance (DeFi).</p>
+                </div>
+            </div>
+            <div class="third-section">
+                <div class="text-content">
+                    <h2 class="mb3">Sophisticated Smart Contracts<br> for Defi and GameDefi</h2>
+                    <p class="font-bold mb5">Digital innovation for a changing world</p>
+                    <p class="mb5">Using Solidity programming language and other latest technologies, we develop
+                        scalable, intelligent, stand-alone,<br> technologically advanced applications for Decentralized
+                        Finance (DeFi) and GameFi.</p>
+                    <p class="mb5">
+                        Our experienced team delivers solutions to innovate crypto currency lending platforms, decentralized
+                        exchanges (DEX)<br> powered by automated market makers (AMM) or NFT marketplaces using a variety of
+                        Blockchain Development<br> Platforms like Ethereum, Tezos, XDC Network, Corda, Stellar, Hyperledger
+                        and others.
+                    </p>
+                </div>
+                <div class="btn-container pt5">
                     <a href="#" class="button ribbon-outset border">GET IN TOUCH</a>
                 </div>
-
             </div>
-        </div>
-        <div class="second-section">
-            <img id="controller-container" src="{{ url('images/Shape_21.png') }}" alt="">
-            <img id="img_1" src="{{ url('images/controller_1.png') }}" alt="">
-            <div class="text-content">
-                <h3 class="mb5">Blockchain GameFi<br>
-                    Development</h3>
-                <p>Automata develops games that combine video games with blockchain technology, cryptocurrencies and
-                    decentralized finance also known as GameFi.</p>
-                <p>With focus on play-to-earn (P2E), we are developing in-game economies using cryptocurrencies,
-                    non-fungible tokens (NFTs), in-game assets, and elements of decentralized finance (DeFi).</p>
+            <div class="fourth-section">
+                <img id="controller-container" src="{{ url('images/Shape_22.png') }}" alt="">
+                <img id="img_1" src="{{ url('images/Layer_3.png') }}" alt="">
+                <div class="text-content">
+                    <h3 class="mb5">Blockchain GameFi<br>
+                        Development</h3>
+                    <p>Automata develops games that combine video games with blockchain technology, cryptocurrencies and
+                        decentralized finance also known as GameFi.</p>
+                    <p>With focus on play-to-earn (P2E), we are developing in-game economies using cryptocurrencies,
+                        non-fungible tokens (NFTs), in-game assets, and elements of decentralized finance (DeFi).</p>
+                </div>
             </div>
-        </div>
-        <div class="third-section">
-            <div class="text-content">
-                <h2 class="mb3">Sophisticated Smart Contracts<br> for Defi and GameDefi</h2>
-                <p class="font-bold mb5">Digital innovation for a changing world</p>
-                <p class="mb5">Using Solidity programming language and other latest technologies, we develop
-                    scalable, intelligent, stand-alone,<br> technologically advanced applications for Decentralized
-                    Finance (DeFi) and GameFi.</p>
-                <p class="mb5">
-                    Our experienced team delivers solutions to innovate crypto currency lending platforms, decentralized
-                    exchanges (DEX)<br> powered by automated market makers (AMM) or NFT marketplaces using a variety of
-                    Blockchain Development<br> Platforms like Ethereum, Tezos, XDC Network, Corda, Stellar, Hyperledger
-                    and others.
-                </p>
-            </div>
-            <div class="btn-container pt5">
-                <a href="#" class="button ribbon-outset border">GET IN TOUCH</a>
-            </div>
-        </div>
-        <div class="fourth-section">
-            <img id="controller-container" src="{{ url('images/Shape_22.png') }}" alt="">
-            <img id="img_1" src="{{ url('images/Layer_3.png') }}" alt="">
-            <div class="text-content">
-                <h3 class="mb5">Blockchain GameFi<br>
-                    Development</h3>
-                <p>Automata develops games that combine video games with blockchain technology, cryptocurrencies and
-                    decentralized finance also known as GameFi.</p>
-                <p>With focus on play-to-earn (P2E), we are developing in-game economies using cryptocurrencies,
-                    non-fungible tokens (NFTs), in-game assets, and elements of decentralized finance (DeFi).</p>
-            </div>
-        </div>
-        {{-- <div class="second-section">
+            {{-- <div class="second-section">
             <div class="container_1">
                 <img id="controller" src="{{ url('images/Group_33.png') }}" alt="">
                 <div class="text-content">
@@ -102,7 +103,11 @@
                 </div>
             </div>
         </div> --}}
-        {{-- <div class="third-section">
+            {{-- {{-- <div class="third-section"> --}}
+        </div>
+    </div>
+@endsection
 
-        </div> --}}
-    </div @endsection @section('pagejs') <script src="{{ asset('js/pages/guests/home/index.js') }}"></script> @stop
+@section('pagejs')
+    <script src="{{ asset('js/pages/guests/home/index.js') }}"></script>
+@stop
